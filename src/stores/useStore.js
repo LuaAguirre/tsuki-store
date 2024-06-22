43123623836e1b3bf8a26ca6d69aadef0f0b3aed
LuaAguirre <renatoag98@gmail.com>
2024-06-21 23:34:44 -0500

    cart logic check

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -3,8 +3,26 @@ import { create } from "zustand";
 const useStore = create((set) => ({
     cart: [],
     totalAmount: 0,
-    setCart: product => set((state) => ({ cart: [...state.cart, product] })),
+    setCart: product => set((state) => {
+        const productInCartIndex = state.cart.findIndex(item => item.product.id === product.id)
+
+        if (productInCartIndex >= 0) {
+            const newCart = structuredClone(state.cart)
+            newCart[productInCartIndex].quantity += 1
+
+            return {
+                cart: newCart
+            }
+        }
+
+        return {
+            cart: [...state.cart, { product, quantity: 1 }]
+        }
+    }),
     clearCart: () => set(state => ({ ...state, cart: [], totalAmount: 0 }))
 }))
 
 export { useStore }
+
+//manera basica de agregar un producto al carrito
+//setCart: product => set((state) => ({ cart: [...state.cart, product] })),
