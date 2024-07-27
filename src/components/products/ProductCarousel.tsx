4e873f79a4995dcafd143c57ab4f35f2cd324315
LuaAguirre <renatoag98@gmail.com>
2024-07-27 02:28:07 -0500

    Lazy load carousel

--- src/components/products/ProductCarousel.tsx
+++ src/components/products/ProductCarousel.tsx
@@ -6,18 +6,29 @@ import {
   CarouselPrevious
 } from '@/components/ui/carousel'
 import type { Product } from '@/types/product'
+import { useState } from 'react'
 
 export function ProductCarousel({ product }: { product: Product }) {
+  // Extra images loading
+  const [areImagesLoading, setAreImagesLoading] = useState(false)
+
+  const handleMouseEnter = () => {
+    // Start loading additionals images
+    setAreImagesLoading(true)
+  }
+
   return (
-    <Carousel>
+    //onMouseEnter for the hover
+    <Carousel onMouseEnter={handleMouseEnter}>
       <CarouselContent>
         {product.images.map((image: string, index: number) => (
           <CarouselItem key={index}>
             <div className='rounded-md p-1 h-96 overflow-hidden'>
               <img
-                src={image}
+                src={index === 0 || areImagesLoading ? image : undefined}
                 alt={`Imagen ${index + 1} del producto`}
-                className='rounded-md size-90 '
+                className='rounded-md size-90'
+                loading={index === 0 ? 'eager' : 'lazy'}
               />
             </div>
           </CarouselItem>
