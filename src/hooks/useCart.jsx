aedcc15655876a826908fa1a5710d6b22eaacf1a
LuaAguirre <renatoag98@gmail.com>
2024-07-29 01:10:40 -0500

    tailwind-sort

--- src/hooks/useCart.jsx
+++
@@ -1,9 +0,0 @@
-import { useCartStore } from '@/stores/useCartStore'
-import { useFilterStore } from '@/stores/useFilterStore'
-
-export function useCart() {
-  const setCart = useCartStore((state) => state.setCart)
-  const filteredProducts = useFilterStore((state) => state.filteredProducts)
-
-  return { setCart, filteredProducts }
-}
